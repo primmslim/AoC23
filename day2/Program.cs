9a4d5cbf8972abd4e34d13ca8fd28e623b3f8419
cody <primmslimnv@gmail.com>
2023-12-03 19:40:22 +1300

    initial

---
+++ day2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.ExceptionServices;
+
+var lines = File.ReadAllLines(@"prod1.txt");
+
+
+var cuebMax = new Dictionary<string,int>(){
+    {"red",12},
+    {"green",13},
+    {"blue",14}
+};
+
+
+var badGames = new List<int>();
+int totalPower = 0;
+
+
+foreach(string line in lines){
+    int gameNumber = int.Parse(line.Split(':')[0].Split(' ')[1]);
+    string games = line.Split(':')[1];
+    string[] rounds = games.Split(';');
+
+        var roundTotals = new Dictionary<string,List<int>>(){
+            {"red", new List<int>()},
+            {"green", new List<int>()},
+            {"blue", new List<int>()},
+        };
+    
+
+    foreach(string round  in rounds){
+
+        //3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        string[] hands = round.Split(',');
+        foreach(string hand in hands){
+            //3 blue, 4 red
+            // 1 red, 2 green, 6 blue
+            // 2 green
+            foreach(string cubesResult in  hand.Split(',') ){
+                int num = int.Parse(cubesResult.Trim().Split(' ')[0]);
+                string col = cubesResult.Trim().Split(' ')[1];
+                roundTotals[col].Add(num);
+            }
+        }
+
+
+    }
+    var maxes = roundTotals.Values.Select(l => l.Max());
+    var power = maxes.Aggregate((i,v) => i * v );
+
+    System.Console.WriteLine($"Game {gameNumber}: Power {power} Mins: {string.Join(',' , maxes) }");
+    totalPower += power;
+}
+
+System.Console.WriteLine($"TotalPower: {totalPower}");
+
+
+
---
+++ day3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("p1.txt");
+
+var Parts = new List<(int row, int col, string val)>();
+
+for (int i = 0; i < lines.Length; i++)
+{
+    string line = lines[i];
+    //get nums
+    foreach(Match match in Regex.Matches(line,@"([^\w^\.])|([0-9]*)")){
+        if (match.Length>0)
+            Parts.Add((row:i, col: match.Index, val: match.Value));
+    }
+}
+
+var symbols = Parts.Where(p => !p.val.All(c => char.IsNumber(c)));
+
+// abcd      --case 1 
+//    efgh
+// ab
+//       vas   --case 2
+
+var MatchingParts = new List<(int row, int col, string val)>();
+var Gears = new List<(int row, int col, string val)>();
+int totalPower = 0;
+
+foreach(var symbol in symbols){
+    var matchesToSymbol = Parts.Where(p => 
+    p.row >= symbol.row -1 && //row below
+    p.row <= symbol.row +1 && //row above
+    p.col + p.val.Length -1 >= symbol.col -1 && //case 1
+    p.col  <= symbol.col  + 1 // case 2
+    ).Where(m => !symbols.Select(s => s.val)
+                            .Contains(m.val));
+
+    foreach (var match in matchesToSymbol)
+    {
+        if (!MatchingParts.Contains(match))
+            MatchingParts.Add(match);
+    }
+        //check if gear with 2 non symbol matches
+        if (symbol.val.Equals("*") && matchesToSymbol.Count() == 2){
+            int ratio = matchesToSymbol.Select(m => int.Parse(m.val))
+                                    .Aggregate((a,m) =>  a * m);
+            totalPower += ratio;
+        }
+  
+
+    
+}
+
+int total = MatchingParts.Sum(m => int.Parse(m.val));
+
+
+System.Console.WriteLine(total);
+System.Console.WriteLine(totalPower);
+
+
+
+

9bf0e74a67e8e059cb7b5af77b09390bed13687f
cody <primmslimnv@gmail.com>
2023-12-04 19:53:52 +1300

    day 4

---
+++ day4/Program.cs
@@ -0,0 +1,35 @@
+﻿var lines = File.ReadAllLines("data.txt");
+
+int total = 0;
+var games = new List<(int num,int plays, string data)>().ToArray();
+Array.Resize(ref games,lines.Length);
+for (int i = 0; i < lines.Length; i++)
+{
+    games[i] = new (i,1,lines[i]);
+}
+
+
+for (int i = 0; i < games.Length; i++)
+{
+    string gameData = games[i].data[(games[i].data.IndexOf(":")+2) ..];
+    string[] winningNumbers = gameData.Split('|')[0].Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+    string[] myNumbers = gameData.Split('|')[1].Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+    int wins = myNumbers.Intersect(winningNumbers).Count();
+
+   // if (wins == 0) games[i].plays = 0;
+
+
+    for (int j = 0; j < games[i].plays; j++)
+    {
+        for (int k = 0; k < wins; k++)
+        {
+            games[i+k+1].plays += 1;
+            
+        }
+    }
+
+    
+}
+
+System.Console.WriteLine(games.Sum(g => g.plays));
